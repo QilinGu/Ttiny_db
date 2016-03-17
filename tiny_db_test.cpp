0c52072cf4189589dd0325fee40ebc31ca1531db
zhuokaijia <946114150@qq.com>
2016-03-17 12:39:12 +0800

    Update tiny_db_test.cpp

--- tiny_db_test.cpp
+++ tiny_db_test.cpp
@@ -1,15 +1,38 @@
 #include<iostream>
 #include<string>
+#include<sstream>
+#include<cassert>
 #include"tiny_db_DB.h"
 
+using std::cout;
+using std::endl;
 
-int main(int argc,char **argv){
+std::string int_to_string(int a){
+	std::stringstream ss;
+	ss << a;
+	return ss.str();
+}
+
+int main(int argc, char **argv){
 	tiny_db::DB db;//=new tiny_db::DB();
-	std::string sl = "d";
-	db.insert(sl, sl);
-	db.insert("assd", "fd");
-	db.insert("aasd", "fd");
-	std::string s = db.getValue("aasd");
-	std::cout << s;
-	std::cin >> s;
+	for (int i = 1; i < 1000000; i++){
+		std::string str = int_to_string(i);
+		db.insert(str, str);
+		cout << "insert : " << i << endl;
+	}
+	
+	for( int i = 100; i < 1000000; i++){
+		std::string str = int_to_string(i);
+		std::string temp = db.getValue(str);
+		assert(str == temp);
+		cout<<"getValue : "<<str<<endl;
+	}
+
+	for (int i = 100; i < 5000; i++){
+		std::string str = int_to_string(i);
+		db.deleteKey(str);
+		std::string temp = db.getValue(str);
+		assert(temp !=str);
+		cout << "deleted succeed " << i << endl;
+	}
 }
